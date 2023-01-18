6da32ed28e00a476b01ac39830e7d870b64f6827
YeenYan <markiantalan@gmail.com>
2023-01-18 09:37:21 +0800

    add geolocation/map

--- script.js
+++ script.js
@@ -10,3 +10,50 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// Geolocation API
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      // from Leaflet.com
+      // set the map where your current location is
+      const map = L.map('map').setView(coords, 13); // 13 is how close the zoom is, the higher the value the closer it gets
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
+
+
+      // Event listener
+      // take coordinates whenever & wherever the user click on the map
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+
+        const newCoords = [lat, lng]
+
+        L.marker(newCoords)
+          .addTo(map)
+          .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup'
+          }))
+          .setPopupContent('Workout')
+          .openPopup();
+      })
+
+
+    }, function () {
+      alert(`Could not get your position`);
+    })
+}
